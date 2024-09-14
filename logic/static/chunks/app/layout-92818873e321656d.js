37b1eef6f2734242cba606370926b1ef46b1735f
Williams Gutierrez Ch. <82471475+wil-deving@users.noreply.github.com>
2024-09-14 00:07:50 -0400

    RELEASE v1.3

---
+++ logic/static/chunks/app/layout-92818873e321656d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7313:function(n,e,r){Promise.resolve().then(r.bind(r,797))},797:function(n,e,r){"use strict";r.r(e),r.d(e,{Providers:function(){return i}});var t=r(3827),u=r(9953);function i(n){let{children:e}=n;return(0,t.jsx)(u.x,{children:e})}}},function(n){n.O(0,[951,953,971,69,744],function(){return n(n.s=7313)}),_N_E=n.O()}]);
